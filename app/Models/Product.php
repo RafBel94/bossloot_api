97fa189344cb395dced0fe79eb2dfd041cbfc379
RafBel94 <beltran.caraf23@cadiz.salesianos.edu>
2025-03-28 22:28:43 +0100

    Fixed Product related classes for correct relations

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -24,19 +24,74 @@ class Product extends Model
 
     public function specs()
     {
-        return match($this->category) {
-            'ram' => $this->hasOne(RamSpec::class),
-            'gpu' => $this->hasOne(GpuSpec::class),
-            'cpu' => $this->hasOne(CpuSpec::class),
-            'motherboard' => $this->hasOne(MotherboardSpec::class),
-            'storage' => $this->hasOne(StorageSpec::class),
-            'psu' => $this->hasOne(PsuSpec::class),
-            'case' => $this->hasOne(CaseSpec::class),
-            'cooler' => $this->hasOne(CoolerSpec::class),
-            'display' => $this->hasOne(DisplaySpec::class),
-            'keyboard' => $this->hasOne(KeyboardSpec::class),
-            'mouse' => $this->hasOne(MouseSpec::class),
-            default => null,
-        };
+        $relations = [
+            'ram' => $this->ramSpec(),
+            'gpu' => $this->gpuSpec(),
+            'cpu' => $this->cpuSpec(),
+            'motherboard' => $this->motherboardSpec(),
+            'storage' => $this->storageSpec(),
+            'psu' => $this->psuSpec(),
+            'case' => $this->caseSpec(),
+            'cooler' => $this->coolerSpec(),
+            'display' => $this->displaySpec(),
+            'keyboard' => $this->keyboardSpec(),
+            'mouse' => $this->mouseSpec(),
+        ];
+
+        return $relations[$this->category] ?? null;
+    }
+
+    public function ramSpec()
+    {
+        return $this->hasOne(RamSpec::class);
+    }
+
+    public function gpuSpec()
+    {
+        return $this->hasOne(GpuSpec::class);
+    }
+    public function cpuSpec()
+    {
+        return $this->hasOne(CpuSpec::class);
+    }
+
+    public function motherboardSpec()
+    {
+        return $this->hasOne(MotherboardSpec::class);
+    }
+
+    public function storageSpec()
+    {
+        return $this->hasOne(StorageSpec::class);
+    }
+
+    public function psuSpec()
+    {
+        return $this->hasOne(PsuSpec::class);
+    }
+
+    public function caseSpec()
+    {
+        return $this->hasOne(CaseSpec::class);
+    }
+
+    public function coolerSpec()
+    {
+        return $this->hasOne(CoolerSpec::class);
+    }
+
+    public function displaySpec()
+    {
+        return $this->hasOne(DisplaySpec::class);
+    }
+
+    public function keyboardSpec()
+    {
+        return $this->hasOne(KeyboardSpec::class);
+    }
+
+    public function mouseSpec()
+    {
+        return $this->hasOne(MouseSpec::class);
     }
 }
